f2dc61aa6e1592ed4e294ee38a844ce54a415131
Martin Wittlinger <wittlinger.martin@gmail.com>
2023-10-30 01:30:12 +0100

    Add Quarkus Quartz dependency and update fork syncing schedule.

--- src/main/java/io/github/martinwitt/repo_syncer/Application.java
+++ src/main/java/io/github/martinwitt/repo_syncer/Application.java
@@ -1,9 +1,8 @@
 package io.github.martinwitt.repo_syncer;
 
 import io.quarkus.logging.Log;
-import io.quarkus.runtime.StartupEvent;
+import io.quarkus.scheduler.Scheduled;
 import io.vertx.core.Vertx;
-import jakarta.enterprise.event.Observes;
 import java.time.Duration;
 import org.kohsuke.github.GHRepository;
 
@@ -20,24 +19,18 @@ public class Application {
     this.vertx = vertx;
   }
 
-  void startup(@Observes StartupEvent event) {
-    Log.info("Application started");
-
-    vertx.setPeriodic(
-        Duration.ofMinutes(5).toMillis(),
-        duration,
-        id -> {
-          var result = getForks.getAllForks();
-          Log.info("Syncing %d forks".formatted(result.size()));
-          long start = System.currentTimeMillis();
-          int count = 0;
-          for (GHRepository repository : result) {
-            if (syncFork.syncFork(repository)) {
-              count++;
-            }
-          }
-          long end = System.currentTimeMillis();
-          Log.info("Synced %d forks in %d ms".formatted(count, end - start));
-        });
+  @Scheduled(every = "6h")
+  public void updateForks() {
+    var result = getForks.getAllForks();
+    Log.info("Syncing %d forks".formatted(result.size()));
+    long start = System.currentTimeMillis();
+    int count = 0;
+    for (GHRepository repository : result) {
+      if (syncFork.syncFork(repository)) {
+        count++;
+      }
+    }
+    long end = System.currentTimeMillis();
+    Log.info("Synced %d forks in %d ms".formatted(count, end - start));
   }
 }
